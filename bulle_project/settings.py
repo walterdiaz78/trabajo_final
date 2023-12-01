91b7fdeaf6d1f8a4091df097e27eca5cb6aad90e
Walter Diaz <32580430+walterdiaz78@users.noreply.github.com>
2023-12-01 05:58:32 +0000

    mysql config added

--- bulle_project/settings.py
+++ bulle_project/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = "bulle_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "walterdiaz78$bulle_project",
+        "USER": "walterdiaz78",
+        "PASSWORD": "mysql123",
+        "HOST": "walterdiaz78.mysql.pythonanywhere-services.com",
+        "PORT": "3306",
     }
 }
 
